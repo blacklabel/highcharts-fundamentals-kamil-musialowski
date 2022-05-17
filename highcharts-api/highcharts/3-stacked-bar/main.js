aaa82f84af3d8d0ae8bc57e36d732c65f4d9d88b
Kamil <kamil.musialowski@blacklabel.pl>
2022-05-17 10:11:55 +0200

    Task 3 finished

--- highcharts-api/highcharts/3-stacked-bar/main.js
+++ highcharts-api/highcharts/3-stacked-bar/main.js
@@ -2,6 +2,7 @@ const chart = Highcharts.chart('container', {
 
     chart: {
         type: 'bar',
+        spacingTop: 100,
     },
 
     legend: {
@@ -28,7 +29,6 @@ const chart = Highcharts.chart('container', {
         title: {
             text: 'Amount',
         },
-
         tickInterval: 50,
         stackLabels: {
             enabled: true,
@@ -55,4 +55,45 @@ const chart = Highcharts.chart('container', {
             data: [100, 100, 60, 40],
         },
     ],
+}, function (chart) {
+    const ren = chart.renderer;
+    generateButtons(chart, ren);
+    addLabels(chart, ren);
 });
+
+function generateButtons(chart, ren) {
+    const pointsArr = chart.series[0].points;
+
+    pointsArr.forEach(point => {
+        const x = chart.yAxis[0].toPixels(350),
+            y = chart.plotHeight + chart.plotTop - point.plotX; // Get a bar width center 
+        // Create and add a button with position 0, 0
+        const btn = ren.button('How to fix', 0, 0, null, {
+            // Theme settings
+            stroke: 'blue',
+            'stroke-width': 3,
+            fill: 'transparent',
+        }).add();
+        // Position the button on yAxis and center it depending on its height
+        btn.attr({
+            x: x,
+            y: y - btn.getBBox().height / 2,
+        });
+    })
+}
+
+function addLabels(chart, ren) {
+    const x = chart.yAxis[0].toPixels(350);
+
+    function createLabel(text, x, y = 80) {
+        ren.text(text, x, y).css({
+            color: 'black',
+            fontSize: 20
+        }).add()
+    }
+
+    createLabel('Issue', 20);
+    createLabel('Record Count', 120);
+    createLabel('Action', x);
+}
+
