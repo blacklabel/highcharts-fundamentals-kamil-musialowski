27b922f3a44704475a48ebe19239804934acde12
Kamil <kamil.musialowski@blacklabel.pl>
2022-06-09 10:26:35 +0200

    Formatting fixed

--- highcharts-api/highcharts/11-click-mousemove-cursor/main.js
+++ highcharts-api/highcharts/11-click-mousemove-cursor/main.js
@@ -3,24 +3,23 @@ Highcharts.chart('container', {
     series: [{ data: [10, 13, 21, 40, 50, 33, 65, 21, 22] }]
 
 }, function (chart) {
-
     const chartContainer = chart.container,
         ren = chart.renderer,
-        customMouseDot = ren.circle(-5, 0, 5).attr({ fill: 'blue' }).add()
+        customMouseDot = ren.circle(-5, 0, 5).attr({ fill: 'blue' }).add();
 
     function moveMouseDot(event) {
         const x = event.chartX,
             y = event.chartY
-        customMouseDot.attr({ x, y })
+        customMouseDot.attr({ x, y });
     }
 
     function addCustomDot(event) {
         const x = event.chartX,
             y = event.chartY
-        ren.circle(x, y, 5).attr({ fill: 'blue' }).add()
+        ren.circle(x, y, 5).attr({ fill: 'blue' }).add();
     }
 
-    chartContainer.addEventListener('mousemove', moveMouseDot)
-    chartContainer.addEventListener('click', addCustomDot)
-})
+    chartContainer.addEventListener('mousemove', moveMouseDot);
+    chartContainer.addEventListener('click', addCustomDot);
+});
 
